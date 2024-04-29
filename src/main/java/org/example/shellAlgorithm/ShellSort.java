d0a1c1133b7869d8ff2c085dd1421f3e033736b2
kirillgutorov <gutorov.k@mail.ru>
2024-04-29 22:55:33 +0700

    ShellSort

---
+++ src/main/java/org/example/shellAlgorithm/ShellSort.java
@@ -0,0 +1,38 @@
+package org.example.shellAlgorithm;
+
+public class ShellSort {
+    public static void shellSort(int[] arr) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 54, 2, 3};
+
+        System.out.println("Array before sorting:");
+        printArray(arr);
+
+        shellSort(arr);
+
+        System.out.println("\nArray after sorting:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
+
